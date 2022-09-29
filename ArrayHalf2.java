187f7c18e7f1386660f9f29d0fac9b031a9268fa
surajlangute <surajlangute55@gmail.com>
2022-09-29 15:20:00 +0530

    Revision for for-loop

---
+++ ArrayHalf2.java
@@ -0,0 +1,14 @@
+package forloop.problems;
+
+public class ArrayHalf2 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = i; j <= 5; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+}
